ac7a60926e2ff9e0c830ccd2eacb9c9151c1e4d2
orion-yang <osy29405@uga.edu>
2022-03-27 21:46:21 -0400

    first commit

---
+++ src/main/java/com/tinkerBackend/tinker/TinkerApplication.java
@@ -0,0 +1,13 @@
+package com.tinkerBackend.tinker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinkerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tinkerBackend/tinker/TinkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tinkerBackend.tinker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
